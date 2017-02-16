55ada541002c46bb87cb632e4b927b23bec0cf8f
Denis Romanovsky <dromanovsky@elinext.com>
2017-02-16 11:41:52 +0300

    SMTP and S# config fix for prod.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,26 @@ Rails.application.configure do
 
   # paperclip
   config.paperclip_defaults = {
-      storage: :local
+      storage: :s3,
+      s3_credentials: {
+          bucket: ENV.fetch('S3_BUCKET_NAME'),
+          access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+          secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+          s3_region: ENV.fetch('AWS_REGION')
+      }
+  }
+
+  # SMTP
+  config.action_mailer.default_url_options = { host: 'https://epikformepikthregq.fwd.wf'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      user_name: 'd.romanovskybox@gmail.com',
+      password: 'vbfbqnkugvsglxno',
+      authentication: :login,
+      enable_starttls_auto: true
   }
 
 =begin
