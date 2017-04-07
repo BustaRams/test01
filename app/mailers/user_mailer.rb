e016095e7e46c5bfda779bfe51697cab76f863d1
shantilal-narola <shm.narola@gmail.com>
2017-04-07 09:51:03 +0530

    email template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,10 @@
 class UserMailer < ApplicationMailer
   def enquiry_contact_details(fullname,email,msg,mobile_number)
-    @contact = fullname,email,msg,mobile_number
-    mail( :to => "info@voyajers.com",  :from => email,
+    @name = fullname
+    @email = email
+    @msg = msg
+    @number = mobile_number
+    mail( :to => "jka@narola.email",  :from => email,
     :subject => 'New Inquery for the voyajers' )
   end
 end
